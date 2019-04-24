50f4ad34f71f70cf31af09281e5677d515b80f0b
zvooz <48773078+zvooz@users.noreply.github.com>
2019-04-24 02:21:12 -0400

    unicode

--- portfolios_genisis.py
+++ portfolios_genisis.py
@@ -10,68 +10,68 @@ import datetime
 class Portfolios:
 	epoch = datetime.date(2019, 3, 28)
 
-	# MC = "MC.PA"
-	MC = "LVMHF"
-	# OR = "OR.PA"
-	OR = "LRLCF"
+	# MC = u"MC.PA"
+	MC = u"LVMHF"
+	# OR = u"OR.PA"
+	OR = u"LRLCF"
 
 	# ^BCKY.A or ^BCKYA, the primary ^BCKY portfolio
 	BCKY_A = {
-		"AAPL"	:	5,
-		"ETSY"	:	15,
-		"LULU"	:	6,
+		u"AAPL"	:	5,
+		u"ETSY"	:	15,
+		u"LULU"	:	6,
 		MC		:	3,
 		OR		:	4,
-		"PLNT"	:	15,
-		"SBUX"	:	14,
-		"ULTA"	:	3
+		u"PLNT"	:	15,
+		u"SBUX"	:	14,
+		u"ULTA"	:	3
 	}
 
 	# ^BCKY.B or ^BCKYB, the secondary ^BCKY portfolio, a superset of ^BCKY.A or ^BCKYA
 	BCKY_B = {
-		"COTY"	:	87,
-		"DECK"	:	7,
-		"DEO"	:	6,
-		"EL"	:	6,
-		"LB"	:	36,
-		"FIZZ"	:	18,
-		"NFLX"	:	3,
-		"NKE"	:	12,
-		"SNAP"	:	93,
-		"TGT"	:	12,
-		"TIF"	:	10,
-		"UAA"	:	47,
-		"VFC"	:	11
+		u"COTY"	:	87,
+		u"DECK"	:	7,
+		u"DEO"	:	6,
+		u"EL"	:	6,
+		u"LB"	:	36,
+		u"FIZZ"	:	18,
+		u"NFLX"	:	3,
+		u"NKE"	:	12,
+		u"SNAP"	:	93,
+		u"TGT"	:	12,
+		u"TIF"	:	10,
+		u"UAA"	:	47,
+		u"VFC"	:	11
 	}
 	BCKY_B.update(BCKY_A)
 
 	# ^BCKY.V or ^BCKYV, the Founders Edition
 	BCKY_V = {
 		# US equities, original
-		"AAPL"	:	5,
-		"DECK"	:	7,
-		"DIS"	:	9,
-		"EL"	:	6,
-		"FB"	:	6,
-		"LB"	:	36,
-		"LULU"	:	6,
-		"NKE"	:	12,
-		"SBUX"	:	14,
-		"UAA"	:	47,
-		"ULTA"	:	3,
+		u"AAPL"	:	5,
+		u"DECK"	:	7,
+		u"DIS"	:	9,
+		u"EL"	:	6,
+		u"FB"	:	6,
+		u"LB"	:	36,
+		u"LULU"	:	6,
+		u"NKE"	:	12,
+		u"SBUX"	:	14,
+		u"UAA"	:	47,
+		u"ULTA"	:	3,
 
 		# non-US equities, original
-		"ADDYY"	:	8,
-		"DEO"	:	6,
-		"LRLCY"	:	19,
-		"LVMUY"	:	14,
+		u"ADDYY":	8,
+		u"DEO"	:	6,
+		u"LRLCY":	19,
+		u"LVMUY":	14,
 
 		# community consensus
-		"ETSY"	:	15,
-		"FIZZ"	:	18,
-		"GOOS"	:	21,
-		"NFLX"	:	3,
-		"SNAP"	:	93,
-		"TGT"	:	12,
-		"TIF"	:	10
+		u"ETSY"	:	15,
+		u"FIZZ"	:	18,
+		u"GOOS"	:	21,
+		u"NFLX"	:	3,
+		u"SNAP"	:	93,
+		u"TGT"	:	12,
+		u"TIF"	:	10
 	}
