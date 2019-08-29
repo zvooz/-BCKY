d8d70859f0e78daea310dca0266809bf5c27ed6f
zvooz <48773078+zvooz@users.noreply.github.com>
2019-08-29 19:18:11 -0400

    Update directoryelements.py

--- directoryelements.py
+++ directoryelements.py
@@ -6,8 +6,10 @@ import shlex
 import subprocess
 
 
-
 class DirectoryElements:
+	def __init__(self):
+		pass
+	
 	ckdirs = "bin64/ckdirs" if platform.architecture()[0] == "64bit" else "bin32/ckdirs"
 
 	plots_subdir = "plots"
@@ -16,6 +18,7 @@ class DirectoryElements:
 	BCKY_A_subdir = "^BCKY.A"
 	BCKY_B_subdir = "^BCKY.B"
 	BCKY_V_subdir = "^BCKY.V"
+	RTRD_subdir = "^RTRD"
 
 	cur_dir = os.getcwd()
 	plots_dir = os.path.join(cur_dir, plots_subdir)
@@ -24,12 +27,14 @@ class DirectoryElements:
 	BCKY_A_dir = os.path.join(portfolios_dir, BCKY_A_subdir)
 	BCKY_B_dir = os.path.join(portfolios_dir, BCKY_B_subdir)
 	BCKY_V_dir = os.path.join(portfolios_dir, BCKY_V_subdir)
+	RTRD_dir = os.path.join(portfolios_dir, RTRD_subdir)
 
 	ckdirs_args = shlex.split("\"" + os.path.join(cur_dir, ckdirs)
 		+ "\" -e \"" + EODs_dir
 		+ "\" -A \"" + BCKY_A_dir
 		+ "\" -B \"" + BCKY_B_dir
 		+ "\" -V \"" + BCKY_V_dir
+		+ "\" -R \"" + RTRD_dir
 		+ "\" -p \"" + plots_dir
 		+ "\"")
 	ckdirs_exec = subprocess.Popen(ckdirs_args)
