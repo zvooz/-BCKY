472d8807cb4ef8fcee2c84ff78a825e4f5412e8f
zvooz <48773078+zvooz@users.noreply.github.com>
2019-08-30 17:22:12 -0400

    Update directoryelements.py

--- directoryelements.py
+++ directoryelements.py
@@ -4,38 +4,36 @@ import os
 import platform
 import shlex
 import subprocess
+from portfolios import Portfolios
 
 
 class DirectoryElements:
 	def __init__(self):
 		pass
 	
+	portfolios = Portfolios()
+	
+	ckdirs = u"ckdirs"
+	
 	ckdirs = u"bin64/ckdirs" if platform.architecture()[0] == u"64bit" else u"bin32/ckdirs"
 
 	plots_subdir = u"plots"
 	portfolios_subdir = u"portfolios"
 	EODs_subdir = u"aggregated EODs"
-	BCKY_A_subdir = u"^BCKY.A"
-	BCKY_B_subdir = u"^BCKY.B"
-	BCKY_V_subdir = u"^BCKY.V"
-	RTRD_subdir = u"^RTRD"
+	indices_subdirs = portfolios.indices.keys()
 
 	cur_dir = os.getcwd()
 	plots_dir = os.path.join(cur_dir, plots_subdir)
 	portfolios_dir = os.path.join(cur_dir, portfolios_subdir)
 	EODs_dir = os.path.join(portfolios_dir, EODs_subdir)
-	BCKY_A_dir = os.path.join(portfolios_dir, BCKY_A_subdir)
-	BCKY_B_dir = os.path.join(portfolios_dir, BCKY_B_subdir)
-	BCKY_V_dir = os.path.join(portfolios_dir, BCKY_V_subdir)
-	RTRD_dir = os.path.join(portfolios_dir, RTRD_subdir)
+	indices_dirs = [os.path.join(portfolios_dir, index_subdir) for index_subdir in indices_subdirs]
 
-	ckdirs_args = shlex.split("\"" + os.path.join(cur_dir, ckdirs)
-		+ "\" -e \"" + EODs_dir
-		+ "\" -A \"" + BCKY_A_dir
-		+ "\" -B \"" + BCKY_B_dir
-		+ "\" -V \"" + BCKY_V_dir
-		+ "\" -R \"" + RTRD_dir
-		+ "\" -p \"" + plots_dir
-		+ "\"")
+	ckdirs_args = shlex.split(
+		u"\""
+		+ os.path.join(cur_dir, u"bin64" if platform.architecture()[0] == u"64bit" else u"bin32", ckdirs)
+		+ "\""
+		+ u' '.join([u"\"" + index_dir + u"\"" for index_dir in indices_subdirs])
+	)
+	
 	ckdirs_exec = subprocess.Popen(ckdirs_args)
 	ckdirs_exec.communicate()
